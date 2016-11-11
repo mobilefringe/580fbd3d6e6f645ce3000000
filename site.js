f2d0a03f18f15943a17cb3db6a02fedacdee4a99
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-11-11 16:01:24 -0500

    Downtown Silverspring 1.0  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json = {
                     "mall_name" : "Downtown Silver Spring",
-                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1455899596000/main_logo.png",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/580fbd3d6e6f645ce3000000/image/png/1478203256000/DSS-Logo_no box.png",
                     "time_zone" : "US/Eastern",
                     "newsletter_form_url" : "http://mobilefringe.createsend.com/t/d/s/bitjju/?callback=?",
                     "newsletter_email_field" : "cm-bitjju-bitjju"
