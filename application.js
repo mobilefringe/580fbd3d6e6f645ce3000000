a81a159c2ba32a5cabd2d44f2a277f57f7a5b8c2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-12 21:26:15 -0400

    Downtown Silverspring 1.3  /application.js

--- application.js
+++ application.js
@@ -119,9 +119,10 @@ function renderEvents(container, template, collection){
             val.store_detail_btn = store_details.slug ;
             val.store_name = store_details.name;
             val.image_url = store_details.store_front_url_abs;
-            val.cat_list = store_details.categories.join(',')
-        }
-        else{
+            if(store_details.categories != null){    
+                val.cat_list = store_details.categories.join(',')
+            }
+        } else {
             val.store_name = site_json.mall_name;
             val.image_url = site_json.default_image;
             if(val.logo_url.indexOf('missing.png') > 0){
@@ -135,8 +136,7 @@ function renderEvents(container, template, collection){
         
         if(val.description.length > 200){
             val.description_short = val.description.substring(0,200) + "...";
-        }
-        else{
+        } else {
             val.description_short = val.description
         }
         
@@ -150,8 +150,7 @@ function renderEvents(container, template, collection){
     
         if (start.format("DMY") == end.format("DMY")){
             val.dates = start.format("MMM DD")
-        }
-        else{
+        } else {
             val.dates = start.format("MMM DD") + " - " + end.format("MMM DD")
         }
         
